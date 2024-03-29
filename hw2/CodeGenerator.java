fc73a4d07d5cf45b707e8d88c40638cc8ef01088
kuanyi0226 <kuanyi0226@gmail.com>
2024-03-30 02:35:02 +0800

    hw2 fix

--- hw2/CodeGenerator.java
+++ hw2/CodeGenerator.java
@@ -1,7 +1,7 @@
 public class CodeGenerator {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.err.println("請輸入mermaid檔案名稱");           
+            System.err.println("Please enter the name of mermaid file");           
         }
         else {
             // get input
@@ -17,12 +17,68 @@ public class CodeGenerator {
 }
 class FileReader {
     public String read(String fileName) {
-		return "";		
+		return "";
     }
-    
 }
 class Parser {
-		public static String splitByClass(String input) {
-			return "";
-		}
+    public static String splitByClass(String input) {
+        return "";
+    }
+}
+class FieldComponent{
+    public String access;
+    public String fieldType;
+    public String fieldName;
+    FieldComponent(String access, String fieldType, String fieldName){
+        this.fieldName = fieldName;
+        this.fieldType = fieldType;
+        this.access = access;
+    }
+    public void printField(){
+        System.out.println(access + " " + fieldType + " " + fieldName + ";");
+    }
+}
+class FunctionComponent{
+    public String access;
+    public String functionType;
+    FunctionComponent(String access, String functionType){
+        this.functionType = functionType;
+        this.access = access;
+    }
+    public void printFunction(){
+        String returnValue;
+        if(functionType.equals("int")) returnValue = "0";
+        else if(functionType.equals("String")) returnValue = "\"\"";
+        else if(functionType.equals("boolean")) returnValue = "false";
+        else returnValue = "0";
+        String functionName = functionType + functionType.toLowerCase() + "Function";
+        System.out.println(access + " " + functionName + "() {return " + returnValue + ";}");
+    }
+}
+class GetterSetterComponent{
+    public String access;
+    public String functionType;
+    public boolean isGetter = true; //0: setter, 1: getter
+    public String functionName;
+    GetterSetterComponent(String access, String functionType, boolean isGetter, String functionName){
+        this.functionType = functionType;
+        this.access = access;
+        this.isGetter = isGetter;
+        this.functionName = functionName;
+    }
+    public void printFunction(){
+        String preString = isGetter ? "get" : "set";
+        String nameString = preString + functionName + "()";
+        if(isGetter){
+            //getter
+            System.out.println(access + " " + functionType + " " + nameString + " {");
+            System.out.println("    return" + functionName.toLowerCase() + ";");
+            System.out.println("}");
+        }else{
+            //setter
+            System.out.println(access + " " + functionType + " " + nameString + " {");
+            System.out.println("    this." + functionName.toLowerCase() + ";");
+            System.out.println("}");
+        }
+    }
 }
